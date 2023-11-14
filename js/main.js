71e89c27efdd2b2031ac4e33836b8e00569570cd
Rafal Owczarz <r.j.owczarz@gmail.com>
2023-11-14 19:38:13 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,79 @@
+const menu = document.querySelector('.nav__btns-menu');
+const night = document.querySelector('.nav__btns-night');
+const navList = document.querySelector('.nav__list');
+const nav = document.querySelector('.nav');
+const navImg = document.querySelector('.nav__logo-img');
+const headerBtn = document.querySelectorAll('.header__slider-btn');
+const video = document.querySelector('.header__video');
+const faqs = document.querySelectorAll('.faq__box');
+const profiles = document.querySelectorAll('.testimonials__profile');
+const testimonials = document.querySelectorAll('.testimonials__content');
+let nightCheck;
+
+
+menu.addEventListener('click', () => {
+  menu.classList.toggle('active');
+  navList.classList.toggle('active');
+});
+
+night.addEventListener('click', () => {
+  night.classList.toggle('active');
+  document.body.classList.toggle('dark');
+  if (document.body.classList.contains('dark')) {
+    navImg.src = './assets/img/logo-d.svg';
+    localStorage.setItem('nightChecker', true)
+  } else {
+    navImg.src = './assets/img/logo.svg';
+    localStorage.setItem('nightChecker', false)
+  }
+});
+
+window.addEventListener('load', () => {
+  nightCheck = localStorage.getItem('nightChecker');
+  if (nightCheck === 'true') {
+    document.body.classList.add('dark');
+    navImg.src = './assets/img/logo-d.svg';
+  } else {
+    document.body.classList.remove('dark');
+    navImg.src = './assets/img/logo.svg';
+  }
+});
+
+const headerSlider = manual => {
+  headerBtn.forEach(btn => {
+    btn.classList.remove('active');
+  });
+  headerBtn[manual].classList.add('active');
+  video.src = `./assets/video/header${manual+1}.mp4`;
+  video.classList.remove('active');
+  setInterval(() => {
+    video.classList.add('active');
+  }, 150);
+};
+
+headerBtn.forEach((btn, i) => {
+  btn.addEventListener('click', () => {
+    headerSlider(i);
+  });
+});
+
+faqs.forEach(faq => {
+  faq.addEventListener('click', () => {
+    faq.classList.toggle('active');
+  });
+});
+
+
+profiles.forEach(profile => {
+  profile.addEventListener('click', () => {
+    for (let i = 0; i < profiles.length; i++) {
+      profiles[i].classList.remove('active');
+    }
+    profile.classList.add('active');
+    const order = profile.getAttribute('data-number');
+    testimonials.forEach(testimonial => {
+      testimonial.style.display = 'none';
+    });
+    document.querySelector(`[data-order="${order}"]`).style.display = 'flex';
+  });
+});
---
+++ js/map.js
@@ -0,0 +1,103 @@
+function initMap() {
+  const options = {
+    zoom: 4,
+    center: {
+      lat: 46.00164,
+      lng: 9.70974
+    },
+    mapTypeId: 'hybrid'
+  };
+  const map = new google.maps.Map(document.getElementById('map'), options);
+
+  const addMarker = (coords, info) => {
+    const marker = new google.maps.Marker({
+      position: coords,
+      map: map,
+    });
+
+    const infoWindow = new google.maps.InfoWindow({
+      content: `<h1>${info}</h1><br><a href='#'>Book</a>`
+    });
+
+    marker.addListener('click', () => {
+      infoWindow.open(map, marker);
+    });
+  };
+
+  addMarker({
+    lat: 52.22967,
+    lng: 21.01222
+  }, 'Warsaw');
+  addMarker({
+    lat: 50.07553,
+    lng: 14.4378
+  }, 'Prague');
+  addMarker({
+    lat: 59.32932,
+    lng: 18.06858
+  }, 'Stockholm');
+  addMarker({
+    lat: 59.91386,
+    lng: 10.75224
+  }, 'Oslo');
+  addMarker({
+    lat: 33.97159,
+    lng: -6.84981
+  }, 'Rabat');
+  addMarker({
+    lat: 36.80649,
+    lng: 10.18153
+  }, 'Tunis');
+  addMarker({
+    lat: 37.9838,
+    lng: 23.72753
+  }, 'Athens');
+  addMarker({
+    lat: 39.93336,
+    lng: 32.85974
+  }, 'Ankara');
+  addMarker({
+    lat: 47.49791,
+    lng: 19.04023
+  }, 'Budapest');
+  addMarker({
+    lat: 38.72225,
+    lng: -9.13933
+  }, 'Lisbon');
+  addMarker({
+    lat: 40.41677,
+    lng: -3.70379
+  }, 'Madrid');
+  addMarker({
+    lat: 41.90278,
+    lng: 12.49636
+  }, 'Roma');
+  addMarker({
+    lat: 48.85661,
+    lng: 2.35222
+  }, 'Paris');
+  addMarker({
+    lat: 46.94797,
+    lng: 7.44744
+  }, 'Bern');
+  addMarker({
+    lat: 52.52,
+    lng: 13.40495
+  }, 'Berlin');
+  addMarker({
+    lat: 52.36757,
+    lng: 4.90413
+  }, 'Amsterdam');
+  addMarker({
+    lat: 51.50721,
+    lng: -0.12758
+  }, 'London');
+  addMarker({
+    lat: 53.3498,
+    lng: -6.2603
+  }, 'Dublin');
+  addMarker({
+    lat: 64.14658,
+    lng: -21.94263
+  }, 'Reykjavik');
+}
